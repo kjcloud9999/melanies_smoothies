09f81a54a258a32639ad888af980f58bc3b7aed0
kjcloud9999 <kijammula@deloitte.com>
2024-06-22 20:44:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ if submitted:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
- fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)   
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)   
     #st.write(my_insert_stmt)
     #st.stop()
